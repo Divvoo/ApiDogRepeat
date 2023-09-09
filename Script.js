a364ed24f56177151d48493a53940d3751d6a280
Dawid <dawidrowinski@vp.pl>
2023-09-09 07:48:41 +0200

    Changes

---
+++ Script.js
@@ -0,0 +1,7 @@
+console.log("TEST");
+
+async function logDog () {
+const response = await fetch("https://dog.ceo/api/breeds/image/random"); 
+const dog = await response.json();
+console.log(dog);
+}
