975e05efbb003800c594bcc19be00c51935a6541
Dawid <dawidrowinski@vp.pl>
2023-10-03 17:22:34 +0200

    changee Text

--- Script.js
+++ Script.js
@@ -1,7 +1,15 @@
-console.log("TESTaaaa");
+// console.log("TESTaaaa");
 
 async function logDog () {
-const response = await fetch("https://dog.ceo/api/breeds/image/random"); 
-const dog = await response.json();
-console.log(dog);
+    const response = await fetch("https://dog.ceo/api/breeds/image/random"); 
+    const dog = await response.json();
+    console.log(dog);
+    document.getElementById("demo").innerHTML = "<h1> TEST 12345 </h1>";
+
 }
+
+
+
+
+
+    
