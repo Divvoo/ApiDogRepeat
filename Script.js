2426cf93858cecf249b2ba4f3e50fec4d2070455
Dawid <dawidrowinski@vp.pl>
2023-10-08 22:00:45 +0200

    Add new Branch

--- Script.js
+++ Script.js
@@ -1,10 +1,7 @@
-// console.log("TESTaaaa");
-
 async function logDog () {
     const response = await fetch("https://dog.ceo/api/breeds/image/random"); 
     const dog = await response.json();
-    console.log(dog);
-    document.getElementById("demo").innerHTML = "<h1> TEST 12345 </h1>";
+    document.getElementById("demo").innerHTML = `<img src="${dog.message}">`;
 
 }
 
